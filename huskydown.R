ca9e030cdd38d745c49580a3be468a08804eafc3
Olivia Cattau <ocattau@uw.edu>
2023-06-15 13:25:31 -0700

    added huskjdown

---
+++ huskydown.R
@@ -0,0 +1,3 @@
+if (!require("devtools")) install.packages("devtools", repos = "http://cran.rstudio.org")
+devtools::install_github("rstudio/bookdown")
+devtools::install_github("benmarwick/huskydown")
